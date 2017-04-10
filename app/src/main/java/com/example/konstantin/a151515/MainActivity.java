d189a64a9c6f22365a431b1aa4dd4c289d3a993c
Konstantin <mr.ks83@mail.ru>
2017-04-10 21:47:14 +0300

    add SquareButton class

--- app/src/main/java/com/example/konstantin/a151515/MainActivity.java
+++ app/src/main/java/com/example/konstantin/a151515/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.konstantin.a151515;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.support.v7.widget.GridLayoutManager;
-        import android.support.v7.widget.RecyclerView;
-        import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,33 +25,32 @@ public class MainActivity extends AppCompatActivity {
         gameBoard.setHasFixedSize(true);
         gameBoard.setLayoutManager(layoutManager);
 
-        List<myButton> rowListItem = getAllItemList();
+        List<SquareButton> rowListItem = getAllItemList();
 
         RecyclerViewAdapter RVA=new RecyclerViewAdapter(MainActivity.this,rowListItem);
         gameBoard.setAdapter(RVA);
 
     }
 
-    private List<myButton> getAllItemList(){
+    private List<SquareButton> getAllItemList(){
 
-        List<myButton> allItems = new ArrayList<>();
-        allItems.add(new myButton(MainActivity.this,null,"0",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"1",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"2",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"3",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"4",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"5",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"6",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"7",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"8",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"9",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"10",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"11",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"12",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"13",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"14",R.color.colorAccent));
-        allItems.add(new myButton(MainActivity.this,null,"15",R.color.colorAccent));
-
+        List<SquareButton> allItems = new ArrayList<>();
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
+        allItems.add(new SquareButton(MainActivity.this,null));
         return allItems;
     }
 }
