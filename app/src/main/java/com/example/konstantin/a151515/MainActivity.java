f3d852a1b60f058072d124b51166bad763f6801b
Konstantin <mr.ks83@mail.ru>
2017-04-18 00:24:58 +0300

    add logic, functions spin, generate, onclick interface etc,

--- app/src/main/java/com/example/konstantin/a151515/MainActivity.java
+++ app/src/main/java/com/example/konstantin/a151515/MainActivity.java
@@ -1,56 +1,204 @@
 package com.example.konstantin.a151515;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewDebug;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView gameBoard;
+    TextView step;
+    TextView nameUser;
+
     GridLayoutManager layoutManager;
+
+    int matrix[][] = new int[4][4];
+    private int Etalon[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+    private int countStep=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        layoutManager = new GridLayoutManager(MainActivity.this,4);
+        layoutManager = new GridLayoutManager(MainActivity.this, 4);
 
         gameBoard = (RecyclerView)findViewById(R.id.game_board);
         gameBoard.setHasFixedSize(true);
         gameBoard.setLayoutManager(layoutManager);
 
-        List<SquareButton> rowListItem = getAllItemList();
+        step=(TextView)findViewById(R.id.step);
 
-        RecyclerViewAdapter RVA=new RecyclerViewAdapter(MainActivity.this,rowListItem);
+        Generate();
+        Redraw();
+
+    }
+
+    //-----------------Random Generate --------------------------
+
+    public void Generate() {
+
+        Random generator = new Random();
+        int k;
+        int[] RandomArr = new int[16];
+
+        for (int a = 0; a < 16; a++) RandomArr[a] = -1;//init array arr -1
+
+        do {
+            for (int b = 0; b < 16; b++) {
+
+                do {
+                    k = generator.nextInt(16);// random numb 0-15
+                }
+
+                while (RandomArr[k] != -1);
+
+                RandomArr[k] = b;
+            }
+        }
+        while (!Opportunity(RandomArr));
+
+
+        int c = 0;
+        for (int i = 0; i < 4; i++) {//fill field from rand array
+            for (int j = 0; j < 4; j++) {
+
+                matrix[i][j] = RandomArr[c];
+                c++;
+            }
+        }
+
+    }
+
+    //----------------------  Opportunity  ---------------------
+    public boolean Opportunity(int Array[]) {
+        return true;//if possible fold
+        //return false if impossible
+    }
+
+    //----------------------  Redraw  ---------------------
+
+    private void Redraw() {
+        List<SquareButton> rowListItem = getAllItemList();
+        RecyclerViewAdapter RVA = new RecyclerViewAdapter(MainActivity.this, rowListItem);
         gameBoard.setAdapter(RVA);
 
+        // -----------  onClick for items(with Adapter)   ----------------
+
+        RVA.setOnItemClickListener(new RecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View itemView, int position, String s) {
+                //Toast.makeText(MainActivity.this, "Name " + s + " position " + position, Toast.LENGTH_SHORT).show();
+                spin(Integer.valueOf(s));
+            }
+        });
     }
 
-    private List<SquareButton> getAllItemList(){
+    //----------------- getAllItemList --------------------
+
+    private List<SquareButton> getAllItemList() {
 
         List<SquareButton> allItems = new ArrayList<>();
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
-        allItems.add(new SquareButton(MainActivity.this,null));
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                SquareButton sb = new SquareButton(MainActivity.this, matrix[i][j]);
+                allItems.add(sb);
+            }
+        }
+
         return allItems;
     }
+
+
+    //------------------------- SPIN ----------------------------------
+    private void spin(int num) { //change button on the field
+
+        int i = 0, j = 0;
+
+        for (int k = 0; k < 4; k++) {
+            for (int l = 0; l < 4; l++) {
+                if (matrix[k][l] == num) {
+                    i = k;
+                    j = l;
+
+                }
+            }
+        }
+
+        if (i > 0) {
+            if (matrix[i - 1][j] == 0) {
+                matrix[i - 1][j] = num;
+                matrix[i][j] = 0;
+                changeCounter();
+                Redraw();
+            }
+        }
+
+        if (i < 3) {
+            if (matrix[i + 1][j] == 0) {
+                matrix[i + 1][j] = num;
+                matrix[i][j] = 0;
+                changeCounter();
+                Redraw();
+            }
+        }
+
+        if (j > 0) {
+            if (matrix[i][j - 1] == 0) {
+                matrix[i][j - 1] = num;
+                matrix[i][j] = 0;
+                changeCounter();
+                Redraw();
+
+            }
+        }
+        if (j < 3) {
+            if (matrix[i][j + 1] == 0) {
+                matrix[i][j + 1] = num;
+                matrix[i][j] = 0;
+                changeCounter();
+                CheckWin();
+                Redraw();
+            }
+        }
+
+
+    }
+
+    //-------------------- Check Win -------------------------------
+    public boolean CheckWin() {
+        int e = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (matrix[i][j] != Etalon[e]) {
+                    return false;
+                } else e++;
+            }
+        }
+        return true;
+    }
+
+    //------------------------- Win --------------------------------
+
+    //----------------------- Step Counter ------------------------------
+    public void changeCounter(){
+        countStep++;
+        step.setText(Integer.toString(countStep) + " Step" );
+    }
 }
+
+
--- app/src/main/java/com/example/konstantin/a151515/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/konstantin/a151515/RecyclerViewAdapter.java
@@ -2,13 +2,10 @@ package com.example.konstantin.a151515;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+
 
 import java.util.List;
 
@@ -22,44 +19,69 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     private List<SquareButton> mySquareButtonList;
     private Context context;
 
+//------------------------- interface onClick (main activity(redraw())-----------------------------
+
+    public interface OnItemClickListener{
+        void onItemClick(View itemView, int position, String s);
+    }
+    public static OnItemClickListener listener;
+
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+ //----------------------------------------------------------------------------------
+
     public RecyclerViewAdapter(Context context,List<SquareButton> mySquareButtonList){
         this.mySquareButtonList = mySquareButtonList;
         this.context = context;
 
     }
 
+//---------------------------------------------------------------------------------
     public static class ButtonViewHolders extends RecyclerView.ViewHolder{
 
         //инициализируем View, входящие в RecyclerView.
 
-        public Button b;
+        public SquareButton b;
+
 
         public ButtonViewHolders(View itemView) {
-            super(itemView);
 
-            b=(Button)itemView.findViewById(R.id.squareButton);
+            super(itemView);
+            b=(SquareButton) itemView.findViewById(R.id.squareButton);
 
         }
     }
 
-
+//--------------------------------------onCreate----------------------------------------------
     @Override
     public ButtonViewHolders onCreateViewHolder(ViewGroup parent, int viewType) {
+
         View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.square_button,parent,false);//парсим layout square_button
         ButtonViewHolders BVH=new ButtonViewHolders(v);
         return  BVH;
     }
-
+//---------------------------------------onBind-----------------------------------------------
     @Override
-    public void onBindViewHolder(ButtonViewHolders holder, int position) {
+    public void onBindViewHolder(final ButtonViewHolders holder, final int position) {
 
-        //связываем ButtonViewHolders с myButtonList
-        //holder.b.setBackgroundColor(myButtonList.get(position).bg);
-        // holder.b.setText(myButtonList.get(position).name);
+        holder.b.setText(mySquareButtonList.get(position).name);
+        holder.b.setVisibility(mySquareButtonList.get(position).visibility);
+        final String s=mySquareButtonList.get(position).name;
+        holder.b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RecyclerViewAdapter.listener.onItemClick(v,position,s);
+            }
+        });
+
     }
-
+//--------------------------------------------------------------------------------------
     @Override
     public int getItemCount() {
+
         return mySquareButtonList.size();
     }
 
